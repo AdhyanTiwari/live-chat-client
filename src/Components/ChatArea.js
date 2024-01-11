275b931ceb7e6386125ab1fbcb6d5bf9a0724b67
AdhyanTiwari <130485523+AdhyanTiwari@users.noreply.github.com>
2024-01-11 23:40:52 +0530

    2

--- src/Components/ChatArea.js
+++ src/Components/ChatArea.js
@@ -32,7 +32,7 @@ function ChatArea() {
     };
     axios
       .post(
-        "http://localhost:8080/message/",
+        "https://backend-livechat.onrender.com/message/",
         {
           content: messageContent,
           chatId: chat_id,
@@ -55,7 +55,7 @@ function ChatArea() {
       },
     };
     axios
-      .get("http://localhost:8080/message/" + chat_id, config)
+      .get("https://backend-livechat.onrender.com/message/" + chat_id, config)
       .then(({ data }) => {
         setAllMessages(data);
         setloaded(true);
--- src/Components/Groups.js
+++ src/Components/Groups.js
@@ -36,7 +36,7 @@ function Groups() {
     };
 
     axios
-      .get("http://localhost:8080/chat/fetchGroups", config)
+      .get("https://backend-livechat.onrender.com/chat/fetchGroups", config)
       .then((response) => {
         console.log("Group Data from API ", response.data);
         SetGroups(response.data);
@@ -97,7 +97,7 @@ function Groups() {
                   //   },
                   // };
                   // axios.post(
-                  //   "http://localhost:8080/chat/",
+                  //   "https://backend-livechat.onrender.com/chat/",
                   //   {
                   //     userId: user._id,
                   //   },
